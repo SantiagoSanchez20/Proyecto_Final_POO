a3d7df9fb19884dd1112b78018a65d62fe9356a3
BoteroAlejandro11 <alejo.pichichi@gmail.com>
2025-11-20 17:08:20 -0500

    actulizacion

---
+++ Model/Trampa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Usuario on 20/11/2025.
+//
+
+#include "Trampa.h"
---
+++ Model/Trampa.h
@@ -0,0 +1,13 @@
+//
+// Created by Usuario on 20/11/2025.
+//
+
+#ifndef CLASE18POO_TRAMPA_H
+#define CLASE18POO_TRAMPA_H
+
+
+class Trampa {
+};
+
+
+#endif //CLASE18POO_TRAMPA_H
